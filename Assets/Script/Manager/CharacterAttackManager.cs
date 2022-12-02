3aed53ae38ff1c90a849c6a40e36da425fcb1bd2
Hung Yu <hungyufeather@gmail.com>
2022-12-02 11:28:23 +0800

    feat : Array 改 List

--- Assets/Script/Manager/CharacterAttackManager.cs
+++ Assets/Script/Manager/CharacterAttackManager.cs
@@ -16,11 +16,11 @@ public class CharacterAttackManager : MonoBehaviour
 
     [SerializeField] private List<GameObject> Target;
 
-    CapsuleCollider[] TargetSize;
+    List<CapsuleCollider> TargetSize;
 
     Weapon weaponData;
 
-    PlayerState[] PlayerData;
+    List<PlayerState> PlayerData;
 
     private bool flag;
 
