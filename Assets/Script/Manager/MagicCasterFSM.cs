989295a03d28bdfb11a1d57ec7f23f69ffb6e356
UDDo/Du <serdy3@gmail.com>
2022-12-08 21:04:42 +0800

    法師Fsm _進行中(召喚部分)，其他完成

---
+++ Assets/Script/Manager/MagicCasterFSM.cs
@@ -0,0 +1,326 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MagicCasterState
+{
+    Idle, Trace,Back, Attack, GetHit, Dead,
+}
+public class MagicCasterFSM : MonoBehaviour
+{
+    public GameObject Bug;
+    public GameObject TestCube;
+
+    private MagicCasterState m_NowState;
+    public GameObject Target;
+    public GameObject MySelf;
+    PlayerState State;
+    Animator MubAnimator;
+    int hpTemporary;
+    CapsuleCollider capsule;
+    bool backing;
+    bool tracing;
+    bool InATKrange;
+    bool InATKrange_Close;
+    bool OutATKrange;
+    float TraceRadius;
+    float ATKRadius;
+    float LeaveATKRadius;
+    float Close_ATKRadius;
+
+    float CDs;
+
+    Vector3 GetTargetNormalize;
+    float GetTargetMegnitude;
+    int Count;
+
+    bool LeaveAttackRangeBool;
+    bool InAttackRangeBool;
+    bool RoarBool = false;
+    int FrameCount_Roar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_NowState = MagicCasterState.Idle;
+        capsule = GetComponent<CapsuleCollider>();
+        MubAnimator = GetComponent<Animator>();
+        State = GetComponent<PlayerState>();
+        hpTemporary = State.Hp;
+        FrameCount_Roar = 150;
+        LeaveAttackRangeBool = false;
+        InAttackRangeBool = false;
+
+        ATKRadius = 55;//WeaponÂÐ»\
+
+        Close_ATKRadius = ATKRadius * 0.8f;
+        TraceRadius = ATKRadius * 2;
+        LeaveATKRadius = ATKRadius * 1.2f;
+
+        Count = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (FrameCount_Roar > 0)
+        {
+            FrameCount_Roar--;
+        }
+        if (RoarBool == false)
+        {
+            Roar();
+            RoarBool = true;
+        }
+        if (State.Hp <= 0)//¦º¤`¡÷µLª¬ºA
+        {
+            m_NowState = MagicCasterState.Dead;
+            DeadStatus();
+            return;
+        }
+        else if (State.Hp != hpTemporary)
+        {
+            hpTemporary = State.Hp;
+            MubAnimator.SetBool("GetHit", true);
+        }
+        else if (FrameCount_Roar <= 0)
+        {
+            MubAnimator.SetBool("GetHit", false);
+
+            MubAnimator.SetBool("Warn", false);
+
+            GetTargetNormalize = (Target.transform.position - transform.position).normalized;
+
+            Quaternion Look = Quaternion.LookRotation(GetTargetNormalize);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Look, 14f * Time.deltaTime);
+
+            StartCoroutine(SummonCooldown());
+
+            BugSummon();
+
+            TraceStatus();
+
+            AttackStatus();
+
+            LeaveAttackStatus();
+
+            TooCloseAttackStatus_York();
+
+            BackStatus();
+        }
+        Debug.Log(m_NowState);
+    }
+    public void DeadStatus()
+    {
+        if (m_NowState == MagicCasterState.Dead)
+        {
+            MubAnimator.SetTrigger("Die");
+            capsule.radius = 0f;
+        }
+    }
+    //°lÀ»ª¬ºA
+    public void TraceStatus()
+    {
+        if (LeaveAttackRangeBool == false)
+        {
+            tracing = IsInRange_TraceRange(ATKRadius, MySelf, Target);
+            if (tracing == true)
+            {
+                m_NowState = MagicCasterState.Trace;
+                Debug.Log("NowInT");
+                if (m_NowState == MagicCasterState.Trace)
+                { 
+                    MubAnimator.SetBool("Trace", true);
+                    MubAnimator.SetBool("Attack", false);
+                }
+            }
+            else
+            {
+                MubAnimator.SetBool("Attack", false);
+            }
+        }
+    }
+    //§ðÀ»ª¬ºA
+    public void AttackStatus()
+    {
+        InATKrange = IsInRange_RangedBattleRange(ATKRadius,Close_ATKRadius, MySelf, Target);
+        if (InATKrange == true)
+        {
+            LeaveAttackRangeBool = true;
+            InAttackRangeBool = true;
+            m_NowState = MagicCasterState.Attack;
+            MubAnimator.SetBool("Trace", false);
+            MubAnimator.SetBool("Back", false);
+            Attack();
+            Debug.Log("NowInA");
+        }
+    }
+    //¦b§ðÀ»³J¶À°Ï(¥~°é)ª¬ºA
+    public void LeaveAttackStatus()
+    {
+        if (LeaveAttackRangeBool == true)
+        {
+            OutATKrange = IsOutRange_RangedBattleRange(LeaveATKRadius, ATKRadius, MySelf, Target);
+            if (OutATKrange == true)
+            {
+                m_NowState = MagicCasterState.Idle;
+                Idle();
+                Debug.LogError("L is active");
+            }
+            GetTargetMegnitude = (Target.transform.position - transform.position).magnitude;
+            if (GetTargetMegnitude > LeaveATKRadius)
+            {
+                LeaveAttackRangeBool = false;
+            }
+        }
+    }
+    //¦b§ðÀ»³J¶À°Ï(¤º°é)ª¬ºA
+    public void TooCloseAttackStatus_York()
+    {
+        if (InAttackRangeBool == true)
+        {
+            InATKrange_Close = CloseRange_RangedBattleRange(ATKRadius, Close_ATKRadius, MySelf, Target);
+            if (InATKrange_Close == true)
+            {
+                m_NowState = MagicCasterState.Idle;
+                Idle();
+                Debug.LogError("LI is active");
+            }
+            GetTargetMegnitude = (Target.transform.position - transform.position).magnitude;
+            if (GetTargetMegnitude < Close_ATKRadius)
+            {
+                InAttackRangeBool = false;
+            }
+        }
+    }
+    //«á°hª¬ºA
+    public void BackStatus()
+    {
+        if (InAttackRangeBool == false)
+        {
+            backing = TooCloseRange_RangedBattleRange(Close_ATKRadius, MySelf, Target);
+            if (backing == true)
+            {
+                m_NowState = MagicCasterState.Back;
+                Debug.Log("NowInB");
+                if (m_NowState == MagicCasterState.Back)
+                {
+                    MubAnimator.SetBool("Back", true);
+                    MubAnimator.SetBool("Attack", false);
+                }
+            }
+            else
+            {
+                MubAnimator.SetBool("Attack", false);
+            }
+        }
+    }
+    //¦b»·µ{§ðÀ»½d³ò¤º 
+    public bool IsInRange_RangedBattleRange(float RadiusMax, float RadiusMin, GameObject attacker, GameObject attacked)
+    {
+        Vector3 direction = attacked.transform.position - attacker.transform.position;
+
+        return direction.magnitude <= RadiusMax && direction.magnitude > RadiusMin;
+    }
+    //¦b»·µ{§ðÀ»³J¶À°Ï½d³ò(¥~°é) 
+    public bool IsOutRange_RangedBattleRange(float RadiusMax, float RadiusMin, GameObject attacker, GameObject attacked)
+    {
+        Vector3 direction = attacked.transform.position - attacker.transform.position;
+
+        return direction.magnitude <= RadiusMax && direction.magnitude > RadiusMin;
+    }
+    //¦b»·µ{§ðÀ»³J¶À°Ï½d³ò(¤º°é) 
+    public bool CloseRange_RangedBattleRange(float RadiusMax, float RadiusMin, GameObject attacker, GameObject attacked)
+    {
+        Vector3 direction = attacked.transform.position - attacker.transform.position;
+
+        return direction.magnitude <= RadiusMax && direction.magnitude >= RadiusMin;
+    }
+    //½d³ò§P©w_«á°h
+    public bool TooCloseRange_RangedBattleRange(float Radius, GameObject attacker, GameObject attacked)
+    {
+        Vector3 direction = attacked.transform.position - attacker.transform.position;
+
+        return direction.magnitude < Radius;
+    }
+
+    /*½d³ò§P©w_°lÂÜ*/
+    public bool IsInRange_TraceRange(float Radius, GameObject attacker, GameObject attacked)
+    {
+        Vector3 direction = attacked.transform.position - attacker.transform.position;
+
+        return direction.magnitude > Radius;
+    }
+    public void Roar()
+    {
+        MubAnimator.SetBool("Warn", true);
+    }
+    public void Attack()
+    {
+        if (m_NowState == MagicCasterState.Attack)
+        {
+            MubAnimator.SetBool("Attack", true);
+        }
+        else if (m_NowState == MagicCasterState.Trace)
+        {
+            MubAnimator.SetBool("Attack", false);
+        }
+    }
+    public void Idle()
+    {
+        
+        MubAnimator.SetBool("Trace", false);
+    }
+
+    public void BugSummon()
+    {        
+        if (Count ==0)
+        {            
+            MubAnimator.SetBool("Trace", false);
+            MubAnimator.SetBool("Attack", false);
+            MubAnimator.SetBool("Back", false);
+            MubAnimator.SetBool("GrenerateBug", true);
+            Instantiate(Bug, transform.forward, transform.rotation);
+            Count = 20;
+        }
+        else if (Count!=0)
+        {
+            MubAnimator.SetBool("GrenerateBug", false);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = InATKrange ? Color.red : Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, ATKRadius);
+
+        Gizmos.color = OutATKrange ? Color.blue : Color.black;
+        Gizmos.DrawWireSphere(transform.position, LeaveATKRadius);
+
+        Gizmos.color = InATKrange_Close? Color.green : Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, Close_ATKRadius);
+    }   
+    private void AnimationSpeed_Attack()
+    {
+        MubAnimator.speed = 0.1f;
+    }
+    private void AnimationSpeed_AttackEnd()
+    {
+        MubAnimator.speed = 1f;
+        LeaveATKRadius = ATKRadius * 1.2f;
+        Close_ATKRadius = ATKRadius * .8f;
+    }
+    public void ZoneOpen()
+    {
+        LeaveATKRadius = ATKRadius * 6;
+        Close_ATKRadius = ATKRadius * .1f;
+
+    }
+    IEnumerator SummonCooldown()
+    {
+        while (Count >= 0)
+        {
+            yield return new WaitForSeconds(1);            
+            Count--;
+        }
+    }
+}
