29a1262ef0380f03a2b46e199ca8bcf9de6a1ad7
daniel199602 <daniel19960226@gmail.com>
2022-12-09 20:40:26 +0800

    修正血條抖動

--- Assets/Script/Data/HealthBarFollowing1.cs
+++ Assets/Script/Data/HealthBarFollowing1.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class HealthBarFollowing1 : MonoBehaviour
 {   
     public RectTransform bloodTransform;
+    public RectTransform bloodDelayTransform;
     public RectTransform frameTransform;
     public Transform Head;
     HealthBar M_HealthBar;
@@ -18,23 +19,27 @@ public class HealthBarFollowing1 : MonoBehaviour
         Vector2 HeadPosition = Camera.main.WorldToScreenPoint(Head.position);
        
 
-        bloodTransform.position = HeadPosition;
-        frameTransform.position = HeadPosition;        
+        //bloodTransform.position = HeadPosition;
+        //bloodDelayTransform.position = bloodTransform.position;
+        frameTransform.position = HeadPosition;
 
         //血條超出螢幕        
         if (player2DPosition.x > Screen.width || player2DPosition.x < 0 || player2DPosition.y > Screen.height || player2DPosition.y < 0)
         {
             bloodTransform.gameObject.SetActive(false);
+            bloodDelayTransform.gameObject.SetActive(false);
             frameTransform.gameObject.SetActive(false);
         }
         else
         {
             bloodTransform.gameObject.SetActive(true);
+            bloodDelayTransform.gameObject.SetActive(true);
             frameTransform.gameObject.SetActive(true);
         }
         if (M_HealthBar.Healthbar.fillAmount <= 0)
         {
             bloodTransform.gameObject.SetActive(false);
+            bloodDelayTransform.gameObject.SetActive(false);
             frameTransform.gameObject.SetActive(false);
         }
     }
