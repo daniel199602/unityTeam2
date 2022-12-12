44b240a9b936595580cc01d34142e32d42ccd9a0
daniel199602 <daniel19960226@gmail.com>
2022-12-12 21:42:25 +0800

    新增火把的FOG的顯示

--- Assets/Script/Data/bossTeleport.cs
+++ Assets/Script/Data/bossTeleport.cs
@@ -7,16 +7,25 @@ using UnityEngine.SceneManagement;
 public class bossTeleport : MonoBehaviour
 {
     Animation fadeOut;
+    GameObject Player;
 
+    private float duration;
     void Awake()
     {
         fadeOut = GameObject.Find("FadeColor").GetComponent<Animation>();
-        
+        Player = GameObject.FindWithTag("Player");
     }
     private void OnTriggerEnter(Collider other)
     {
+        
         fadeOut.Play("FadeOut");
         Debug.Log("boss");
         GameManager.Instance().mobPool.Clear();
+        duration = 2;
+        Invoke(nameof(playerclose), duration);
+    }
+    void playerclose()
+    {
+        Player.SetActive(false);
     }
 }
