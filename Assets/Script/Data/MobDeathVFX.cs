587da6f7342471ce6da7179f32af1b3028ba52aa
daniel199602 <daniel19960226@gmail.com>
2022-12-15 19:31:39 +0800

    更改 熊死亡特效消失速度

--- Assets/Script/Data/MobDeathVFX.cs
+++ Assets/Script/Data/MobDeathVFX.cs
@@ -7,8 +7,10 @@ public class MobDeathVFX : MonoBehaviour
     private SkinnedMeshRenderer meshRenderer;
     public GameObject bearMaterial;
     public GameObject FireFVX;
-    
-    public float t = 0.0f;
+    public AnimationCurve fadeIn;
+    public float timer = 0.0f;
+    public float spawnEffectTime = 2;
+
     private void Start()
     {
         meshRenderer = bearMaterial.GetComponent<SkinnedMeshRenderer>();
@@ -16,8 +18,8 @@ public class MobDeathVFX : MonoBehaviour
     void DeathFVXEvent()
     {
         Material[] mats = meshRenderer.materials;
-        t += Time.deltaTime;
-        mats[0].SetFloat("_Cutoff", Mathf.Lerp(0,1,t));
+        timer += Time.deltaTime;
+        mats[0].SetFloat("_Cutoff", fadeIn.Evaluate(Mathf.InverseLerp(0, spawnEffectTime, timer)));
         
         meshRenderer.materials = mats;
        
