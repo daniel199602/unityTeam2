beb3e6d3b9b90ba66c9bec9ffaeda555364ac5f1
daniel199602 <daniel19960226@gmail.com>
2022-12-15 20:53:24 +0800

    修改死亡特效BUG

--- Assets/Script/Data/MobDeathVFX.cs
+++ Assets/Script/Data/MobDeathVFX.cs
@@ -7,25 +7,45 @@ public class MobDeathVFX : MonoBehaviour
     private SkinnedMeshRenderer meshRenderer;
     public GameObject bearMaterial;
     public GameObject FireFVX;
-    public AnimationCurve fadeIn;
-    public float timer = 0.0f;
-    public float spawnEffectTime = 2;
-
+    public GameObject FireFVX2;
+    // public AnimationCurve fadeIn;
+    //public float timer = 0.0f;
+    //public float spawnEffectTime = 1;
+    public float t;
+    public bool isDeath;
     private void Start()
     {
         meshRenderer = bearMaterial.GetComponent<SkinnedMeshRenderer>();
     }
     void DeathFVXEvent()
     {
+        isDeath = true;
+    }
+    void DeathFVX()
+    {
         Material[] mats = meshRenderer.materials;
-        timer += Time.deltaTime;
-        mats[0].SetFloat("_Cutoff", fadeIn.Evaluate(Mathf.InverseLerp(0, spawnEffectTime, timer)));
-        
+        t += Time.deltaTime;
+        mats[0].SetFloat("_Cutoff", Mathf.Lerp(0, 1, t));
+
         meshRenderer.materials = mats;
-       
+    }
+    void DeathEvent_PS()
+    {
         ParticleSystem ps = FireFVX.GetComponent<ParticleSystem>();
         ps.Play();
     }
-    
+    void DeathEvent_PS2()
+    {
+        ParticleSystem ps = FireFVX2.GetComponent<ParticleSystem>();
+        ps.Play();
+    }
+    private void Update()
+    {
+        if(isDeath==true)
+        {
+            DeathFVX();
+        }
+    }
+
 
 }
