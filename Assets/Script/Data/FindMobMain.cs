914ca076a59ff0e29e4fbbd9dac1159a10dce9cf
Hung Yu <hungyufeather@gmail.com>
2022-12-16 14:34:09 +0800

    FindMobMain修正 在Awake 加進怪物池

--- Assets/Script/Data/FindMobMain.cs
+++ Assets/Script/Data/FindMobMain.cs
@@ -5,21 +5,14 @@ using UnityEngine;
 public class FindMobMain : MonoBehaviour
 {
 
-    // Start is called before the first frame update
-
-    //private void Awake()
-    //{
-    //    GameManager.Instance().mobPool.Add(this.gameObject); 
-    //}
-
-    private void Start()
+    private void Awake()
     {
         GameManager.Instance().mobPool.Add(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //private void Start()
+    //{
+    //    GameManager.Instance().mobPool.Add(this.gameObject);
+    //}
+
 }
