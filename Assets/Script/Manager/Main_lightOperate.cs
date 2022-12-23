17e3573cb3a9c7a8d803c34120860c39c6546dce
daniel199602 <daniel19960226@gmail.com>
2022-12-23 16:54:16 +0800

    修改場景上所有物件的影子顯示    修改人物頭上點光源強度與距離與影子    修改場景上火炬的影子打開  平行光改垂直顯示
    修改魔王關前燈光的強度
    
    主角單手劍修改殘影特效第一下位置

--- Assets/Script/Manager/Main_lightOperate.cs
+++ Assets/Script/Manager/Main_lightOperate.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class Main_lightOperate : MonoBehaviour
 {
-    [SerializeField] GameObject goDirectionalLight;
     [SerializeField] GameObject goHandTorch;
-    [SerializeField] GameObject shadow;
-    Light _directionalLight;
     Light _handTorch;
-   // MeshRenderer _shadow;
+   
     
     float handTorchRange;
     Color color0 = new Color (229f/255f, 219f/255f, 219f/255f);
@@ -22,22 +19,20 @@ public class Main_lightOperate : MonoBehaviour
     bool firetest;
     private void Start()
     {
-        _directionalLight = goDirectionalLight.GetComponent<Light>();
+       
         _handTorch = goHandTorch.GetComponent<Light>();
-       // _shadow = shadow.GetComponent<MeshRenderer>();
+      
     }
 
     public void FireFalse()
     {
         _handTorch.intensity = 2;
         _handTorch.range = 35;
-       //_shadow.materials[0].SetFloat("Shadow_A", 0.2f);
     }
     public void FireLight()
     {
-        _handTorch.intensity = 10;
-        _handTorch.range = 100;
-        //_shadow.materials[0].SetFloat("Shadow_A", 0.7f);
+        _handTorch.intensity = 6;
+        _handTorch.range = 80;
     }
     
     void Update()
