aab800da7920d241249beec2aa55abb2cf062032
FEATHER_PC\hungy <hungyufeather@gmail.com>
2022-12-25 02:21:45 +0800

    boss動畫新增斧擊、劍擊音效 上次上傳音效裁減多餘空音

--- Assets/Script/Event/AudioEvent_mobBoss.cs
+++ Assets/Script/Event/AudioEvent_mobBoss.cs
@@ -7,6 +7,11 @@ public class AudioEvent_mobBoss : MonoBehaviour
     AudioSource audioSource;
 
     public AudioClip swingAx;
+    public AudioClip swingMetalSword_1;
+    public AudioClip swingMetalSword_2;
+    public AudioClip fireRoad;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,10 +20,36 @@ public class AudioEvent_mobBoss : MonoBehaviour
     }
 
     /// <summary>
-    /// 火把音效
+    /// 斧擊音效
     /// </summary>
     void PlaySwingAxEvent_swing()
     {
         audioSource.PlayOneShot(swingAx, 1f);
     }
+
+    /// <summary>
+    /// 劍擊音效_1
+    /// </summary>
+    void PlaySwingMetalSwordEvent_1()
+    {
+        audioSource.PlayOneShot(swingMetalSword_1, 1f);
+    }
+
+    /// <summary>
+    /// 劍擊音效_2
+    /// </summary>
+    void PlaySwingMetalSwordEvent_2()
+    {
+        audioSource.PlayOneShot(swingMetalSword_2, 1f);
+    }
+
+    /// <summary>
+    /// 噴火音效
+    /// </summary>
+    void PlayFireRoadEvent()
+    {
+        audioSource.PlayOneShot(fireRoad, 1f);
+    }
+
+
 }
