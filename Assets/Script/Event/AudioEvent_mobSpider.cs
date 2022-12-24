89b422dbb9f2d1e701e995042ea05873b4b91747
FEATHER_PC\hungy <hungyufeather@gmail.com>
2022-12-25 03:23:48 +0800

    蜘蛛音效調整

--- Assets/Script/Event/AudioEvent_mobSpider.cs
+++ Assets/Script/Event/AudioEvent_mobSpider.cs
@@ -6,6 +6,7 @@ public class AudioEvent_mobSpider : MonoBehaviour
 {
     AudioSource audioSource;
     public AudioClip spiderBomb;
+    public AudioClip spiderBrust;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +19,13 @@ public class AudioEvent_mobSpider : MonoBehaviour
     void PlaySpiderBombEvent()
     {
         audioSource.PlayOneShot(spiderBomb, 0.7f);
+    }
+
+    /// <summary>
+    /// 播放蜘蛛叫聲爆炸音效
+    /// </summary>
+    void PlaySpiderBrustEvent()
+    {
+        audioSource.PlayOneShot(spiderBrust, 0.7f);
     }
 }
